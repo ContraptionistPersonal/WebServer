ddd73d5655fcae1352f83b462615755e0a16a849
Ryan Johnson <johnson2004@gmail.com>
2023-09-06 05:50:48 +0000

    first-Commit

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fs = require('fs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,11 +21,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+app.get('/signup', function(req,res){
+
+		res.render("signuppage");
+
+	});
+
+app.get('/submit',function(req,res){
+
+	
+
+	let DBPath = path.join(__dirname,"database/");
+	let fileContent = req.query.name
+	
+	fs.readdir(DBPath,(err,file) =>{
+
+		
+
+
+
+	})
+
+	fs.writeFile(DBPath+"userfile",fileContent,(err)=>{
+		if(err){
+			console.log(err);
+		}
+	})
+	
+})
+/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+*/
+/*
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -37,5 +67,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+*/
 module.exports = app;
